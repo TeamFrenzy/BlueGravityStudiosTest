5ee66ba0012a85ccf5453aef99f529ef3769eda4
TeamFrenzy <gabrielffunes@hotmail.com>
2023-10-18 12:15:41 -0300

    Base Character Sprites & Movement & Tileset
    Added basic character movement, base character sprites, and core tileset. Tileset may be replaced later on.

---
+++ Assets/Scripts/MCAnimator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCAnimator : MonoBehaviour
+{
+    [SerializeField] internal MCController mcController;
+
+    private Animator characterAnimator;
+
+    public bool isMoving;
+
+    private void Start()
+    {
+        characterAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = mcController.horizontalMovement;
+        float vertical = mcController.verticalMovement;
+
+        mcController.motionVector = new Vector2(horizontal, vertical);
+        characterAnimator.SetFloat("Horizontal", horizontal);
+        characterAnimator.SetFloat("Vertical", vertical);
+
+        isMoving = horizontal != 0 || vertical != 0;
+        characterAnimator.SetBool("isMoving", isMoving);
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            characterAnimator.SetFloat("LastHorizontal", horizontal);
+            characterAnimator.SetFloat("LastVertical", vertical);
+        }
+    }
+}
---
+++ Assets/Scripts/MCController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MCController : MonoBehaviour
+{
+    [SerializeField] internal MCAnimator mcAnimator;
+    [SerializeField] float movementSpeed;
+
+    private Rigidbody2D rb;
+
+    public float horizontalMovement;
+    public float verticalMovement;
+
+    public Vector2 motionVector;
+    public Vector2 lastMotionVector;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        if (horizontalMovement != 0 || verticalMovement != 0)
+        {
+            lastMotionVector = new Vector2(horizontalMovement, verticalMovement).normalized;
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = motionVector * movementSpeed;
+    }
+}
